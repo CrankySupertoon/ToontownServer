53e862f04d54f919b4ab716fc07ca4f3d56a39b0
Abrahan N <35606009+zenith110@users.noreply.github.com>
2019-11-10 20:01:35 -0500

    Dependencies update

---
+++ README.md
@@ -0,0 +1,11 @@
+Open source Toontown server built using Toontown Stride source
+
+Features:
+- Custom launcher that allows content pack support
+- Saving multiple IP addresses to favorites
+- Available on Mac, Windows, and Linux
+- Very modular
+
+Credits:
+- Alex : Debugging and feedback
+- Kevin : Debugging and feedback
---
+++ launcher.py
@@ -0,0 +1,394 @@
+# Created by Abrahan Nevarez on August 24th 2019
+# Created for the use of the base game Toontown Stride
+# Can be modified for other servers
+
+import os
+import subprocess
+from sys import platform
+from PySide2 import QtWidgets, QtCore, QtGui
+import shutil
+from pathlib import Path
+from ui import main, CPL, options_menu
+import json
+
+class Options_Menu(options_menu.Ui_Options_Menu, QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(Options_Menu, self).__init__(parent)
+        self.setupUi(self)
+        # Sets up what will be run whenever we load the page
+        # Loads up the content pack tree node
+        self.contentPackExplorer()
+
+        # Loads in our keys and game settings json data
+        self.hotKeySelector()
+        self.optionsJSON()
+
+        # By default, shows an empty option so user can select the option that they want
+        self.gameModes.setCurrentIndex(-1)
+
+        # Handles sava button data
+        self.saveChangesButton.clicked.connect(self.optionsJSON)
+
+        # Login data combobox
+        self.gameModes.currentTextChanged.connect(self.loginSettings)
+        self.treeView.doubleClicked.connect(self.content_pack_opener)
+    
+    # Reads JSON data and dumps it
+    def optionsJSON(self):
+        # Reads the info for the sliders
+        with open('settings/game_settings.json', 'r') as loop:
+            data = json.load(loop)
+
+        # Modifies the content of the sliders and other settings from comboboxes
+        with open('settings/game_settings.json', 'w') as update:
+            data["Graphical Settings"]["Show FPS"] = self.fpsSlider.value()
+            data["Graphical Settings"]["V-Sync"] = self.vSyncSlider.value()
+            data["Graphical Settings"]["Animation Blending"] = self.animationSlider.value()
+            data["Graphical Settings"]["GUI Animation"] = self.guiSlider.value()
+            data["Graphical Settings"]["Particle effects"] = self.particleSlider.value()
+            data["Graphical Settings"]["Disable Accesories"] = self.disableAccessoriesSlider.value()
+            data["Graphical Settings"]["Discord Integration"] = self.discordIntergration.value()
+            data["Login Settings"]["IP Address"] = self.ipAddress.text()
+            data["Login Settings"]["Index"] = self.gameModes.currentIndex()
+            json.dump(data, update, indent = 1)
+        
+    # Loads up the defined hot keys located in settings/controls.json
+    def hotKeySelector(self):
+        with open('settings/controls.json') as loop:
+            data = json.load(loop)
+
+        # Sets the controls from the controls json file
+        self.walkUpButton.setText(data["Controls"]['walk-up'])
+        self.walkLeftButton.setText(data["Controls"]['walk-left'])
+        self.walkRightButton.setText(data["Controls"]['walk-right'])
+        self.walkDownButton.setText(data["Controls"]['walk-down'])
+
+        self.jumpButton.setText(data["Controls"]['jump'])
+
+        self.takeScreenButton.setText(data["Controls"]['screenshot'])
+
+        self.walkButton.setText(data["Controls"]['walk'])
+
+        self.lookUpButton.setText(data["Controls"]['look-up'])
+        self.lookDownButton.setText(data["Controls"]['look-down'])
+
+        self.viewGagsButtons.setText(data["Controls"]['showGags'])
+
+        self.viewToontasksButton.setText(data["Controls"]['showTasks'])
+
+        self.openBookButton.setText(data["Controls"]['stickerBook'])
+
+        self.showAndHideButton.setText(data["Controls"]['toggleGUI'])
+
+        self.viewMapButton.setText(data["Controls"]['showMap'])
+
+        self.openFriendsButton.setText(data["Controls"]['friendsList'])
+
+        self.changeCameraButton.setText(data["Controls"]['cameraNext'])
+
+        self.PreviousCameraButton.setText(data["Controls"]['cameraPrev'])
+
+        self.performActionButton.setText(data["Controls"]['performAction'])
+
+        self.debugScreenButton.setText(data["Controls"]['debugScreenShots'])
+
+        self.displayDebugButton.setText(data["Controls"]['debugInfo'])
+
+        self.cogHQButton.setText(data["Controls"]['cogInfo'])
+
+        self.exitActivityButton.setText(data["Controls"]['exitActivity'])
+
+    def loginSettings(self, index):
+        # Modifies the content of the sliders
+        
+            if index == "Local":
+                print("Local")
+                self.gameModes.setCurrentIndex(0)
+                self.ipAddress.setText("127.0.0.1")
+                self.ipAddress.setReadOnly(True)
+                 
+                # Checks if platform is linux or windows
+                if platform == "linux" or platform == "linux2":
+                    self.playGame.clicked.connect(self.linuxLocalHost)
+        
+                if platform == "win32":
+                    self.playGame.clicked.connect(self.win32LocalHost)
+                
+            elif index == "Server":
+                print("Server")
+                self.gameModes.setCurrentIndex(1)
+                self.ipAddress.setText("")
+                self.ipAddress.setReadOnly(False)
+                if platform == "linux" or platform == "linux2":
+                    self.playGame.clicked.connect(self.linuxServerHost)
+
+                if platform == "win32":
+                    self.playGame.clicked.connect(self.serverHost)
+                
+    
+    # Loads up the content pack folder and lets the user select the content pack
+    # Uses a treeview to show the actual contents of the folder itself
+    def contentPackExplorer(self):
+        path ="content_packs"
+        self.display = QtWidgets.QFileSystemModel()
+
+        self.display.setRootPath((QtCore.QDir.rootPath()))
+
+        self.treeView.setModel(self.display)
+        self.treeView.setRootIndex(self.display.index(path))
+        self.treeView.setSortingEnabled(True)
+
+    
+    # Opens the contents of the zip file and looks for mf file
+    # replaces the contents of the resource folder with the resources provided with the .mf
+    # Will be replaced with a tree node based system
+    def content_pack_opener(self, index):
+        
+        index = self.treeView.currentIndex()
+        file = self.display.filePath(index)
+
+        # Allows us to break the path into multiple segments
+        p = Path(file)
+        Dir_Content_Packs = "./content_packs"
+        Dir_Resources = "./resources"
+
+        # Decompresses the .mf files
+        print("Now unzipping .mf file...please hold")
+        os.chdir("content_packs")
+        content_pack_unzip = subprocess.Popen("multify.exe -x -f " + p.name, shell=False)
+        
+        # Waits till process is done
+        content_pack_unzip.wait()
+
+        print("Okay, I'm done unzipping all the files:")
+        print("Now moving files to resources!")
+        
+        # Search through all folders and files
+        os.chdir("../")
+        for root, folders, files in os.walk(Dir_Content_Packs):
+
+            # Look at each file
+            for contentFile in files:
+
+                # Save the path to the file from the ./content_packs/ folder
+                contentRoot = root
+
+                # Replace the ./content_packs/ path with the ./resources/ path
+                resourceRoot = root.replace(Dir_Content_Packs, Dir_Resources)
+                resourceFilePath = os.path.join(resourceRoot, contentFile)
+
+                # Check whether the directory ./resources/ has that file
+                if os.path.exists(resourceFilePath):
+
+                # If so, replace the file inside ./resources/ with the one from ./content_packs/
+                    contentFilePath  = os.path.join(contentRoot, contentFile)
+                    shutil.copyfile(contentFilePath, resourceFilePath)
+        print("Now cleaning up")
+        print(os.getcwd())
+        # Going back in to remove files that are folders   
+        try:
+            shutil.rmtree(Dir_Content_Packs)
+        except OSError as e:
+            print ("Error: %s - %s." % (e.filename, e.strerror))
+        
+        # Make another directory
+        os.mkdir("content_packs")
+        
+        print("Success! Now launch the game with your new files")
+        
+    # Runs the astron cluster to start the server
+    def startAstronCluster(self):
+        os.chdir("dependencies/astron/")
+
+    # Starts up the AI server
+    def startAiServer(self, districtName, ppython):
+        os.environ["MAX_CHANNELS"] = "999999"
+        os.environ["STATESERVER"] = "4002"
+        os.environ["ASTRON_IP"] = "127.0.0.1:7100"
+        os.environ["EVENTLOGGER_IP"] = "127.0.0.1:7198"
+        os.environ["DISTRICT NAME"] = ""
+        os.environ["BASE_CHANNEL"] = "401000000"
+
+        # Print out echo from batch script
+        print("Starting Toontown Stride AI server...\n ppython:" + ppython + 
+        "\nDistrict name:" + os.environ["DISTRICT NAME"]+ 
+        "\nBase channel" + os.environ["BASE_CHANNEL"] + 
+        "\nMax channels" + os.environ["MAX_CHANNELS"] +
+        "\nAstron IP:" + os.environ["ASTRON_IP"] +
+        "\nEvent Logger IP:" + os.environ["EVENTLOGGER_IP"])
+
+    # Starts up the uberdog server
+    def startUberDog(self, ppython):
+        # Create enviroment variables
+        os.environ["MAX_CHANNELS"] = "999999"
+        os.environ["STATESERVER"] = "4002"
+        os.environ["ASTRON_IP"] = "127.0.0.1:7100"
+        os.environ["EVENTLOGGER_IP"] = "127.0.0.1:7198"
+        os.environ["BASE_CHANNEL"] = "1000000"
+        
+    # Code to log onto a server that has already been started
+    # Added support for JSON loading data
+    def serverHost(self):
+        PPython, randomtext = QtWidgets.QFileDialog.getOpenFileName(self, self.tr("Open ppython file"), self.tr("C:"), self.tr("PPython (*.exe)"))
+        with open('settings/game_settings.json', 'r') as loop:
+        # Grabs the server text and IP to pass into panda
+            with open('settings/game_settings.json') as loop:
+                data = json.load(loop)
+
+            Username = data["Login Settings"]["Username"]
+            IP_Address = data["Login Settings"]["IP Address"]
+
+            # Sets up enviroment variables needed to launch the game
+            os.environ['input'] = '1'
+            os.environ['TTS_GAMESERVER'] = IP_Address
+            os.environ['TTS_PLAYCOOKIE'] = Username
+            with open('settings/game_settings.json', 'w') as update:
+                # Get rid of username and IP address after starting
+                data["Login Settings"]["Username"] = ""
+                data["Login Settings"]["IP Address"] = ""
+                json.dump(data, update, indent = 1)
+
+                subprocess.Popen(PPython + " -m toontown.toonbase.ToontownStart", shell=False)
+            self.close()
+
+    def linuxServerHost(self):
+        os.chdir("../")
+        # Reads our JSON file
+        with open('settings/game_settings.json', 'r') as loop:
+            data = json.load(loop)
+            # Grabs the server text and IP to pass into panda
+
+            Username = data["Login Settings"]["Username"]
+            IP_Address = data["Login Settings"]["IP Address"]
+
+            # Sets up enviroment variables needed to launch the game
+            os.environ['input'] = '1'
+            os.environ['TTS_GAMESERVER'] = IP_Address
+            os.environ['TTS_PLAYCOOKIE'] = Username
+            with open('settings/game_settings.json', 'w') as update:
+                # Get rid of username and IP address after starting
+                data["Login Settings"]["Username"] = ""
+                data["Login Settings"]["IP Address"] = ""
+                json.dump(data, update, indent = 1)
+            subprocess.Popen("ppython -m toontown.toonbase.ToontownStart", shell=False)
+            self.close()
+
+    # Code to start local on linux
+    def linuxLocalHost(self):
+        os.chdir("../")
+
+        os.environ['DYLD_LIBRARY_PATH'] = '`pwd`/Libraries.bundle'
+        os.environ['DYLD_FRAMEWORK_PATH'] = "Frameworks"
+
+        # Reads JSON file
+        with open('settings/game_settings.json', 'r') as loop:
+            data = json.load(loop)
+
+        # Grabs data from JSON file and passes it into username variable for loading
+        Username = data["Login Settings"]["Username"]
+
+        if not Username:
+            QtWidgets.QMessageBox.about(self, "Name required", "Hey! \nYou need a username before we can start!\n")
+            return
+        os.environ['ttsUsername'] = Username
+        os.environ['TTS_PLAYCOOKIE'] = Username
+
+        os.environ['TTS_GAMESERVER'] = "127.0.0.1"
+        with open('settings/game_settings.json', 'w') as update:
+                # Get rid of username and IP address after starting
+                data["Login Settings"]["Username"] = ""
+                data["Login Settings"]["IP Address"] = ""
+                json.dump(data, update, indent = 1)
+        subprocess.Popen("ppython -m toontown.toonbase.ToontownStart")
+        self.close()
+
+    # Code to start local host on windows!
+    def win32LocalHost(self):
+        PPython, randomtext = QtWidgets.QFileDialog.getOpenFileName(self, self.tr("Open ppython file"), self.tr("C:\\"), self.tr("PPython (*.exe)"))
+        if not open(PPython):
+            print("Uh oh! stop this now!")
+        with open('settings/game_settings.json') as loop:
+            data = json.load(loop)
+
+        Username = data["Login Settings"]["Username"]
+
+        backendir = os.chdir("dev/backend/")
+        # Check to prevent a blank user
+        if not Username:
+            QtWidgets.QMessageBox.about(self, "Name required", "Hey! \nYou need a username before we can start!\n")
+            return
+
+        SW_HIDE = 0
+        Info = subprocess.STARTUPINFO()
+
+        Info.dwFlags = subprocess.STARTF_USESHOWWINDOW
+        Info.wShowWindow = SW_HIDE
+
+        subprocess.Popen(r"start-astron-cluster.bat", startupinfo=Info)
+        subprocess.Popen(r"start-ai-server.bat", startupinfo=Info)
+        subprocess.Popen(r"start-uberdog-server.bat", startupinfo=Info)
+
+        ReturnDir = os.chdir("../../")
+
+        os.environ['input'] = '1'
+
+        os.environ['TTS_GAMESERVER'] = "127.0.0.1"
+
+        os.environ['TTS_PLAYCOOKIE'] = Username
+        with open('settings/game_settings.json', 'w') as update:
+                # Get rid of username and IP address after starting
+                data["Login Settings"]["Username"] = ""
+                data["Login Settings"]["IP Address"] = ""
+                json.dump(data, update, indent = 1)
+        subprocess.Popen(PPython + " -m toontown.toonbase.ToontownStart", shell=False)
+        self.close()
+
+    def returnMainMenu(self):
+        self.hide()
+        main_menu = main_window()
+        main_menu.show()
+        main_menu.exec_()
+        main_menu.setWindowModality(QtCore.Qt.WindowModal)
+
+# Initates the basic UI elements
+class main_window(main.Ui_MainWindow, QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(main_window, self).__init__()
+        self.setupUi(self)
+        
+        # Username check to make sure there exist one.
+        if not self.name.text:
+            QtWidgets.QMessageBox.about(self, "IP address", "Hey! \nYou need a name in order to launch the game\n")
+
+    
+        self.options_menu.clicked.connect(self.optionsMenu)
+        print("Directory is currently: " + os.getcwd())
+       
+    # Opens the content pack window
+    def optionsMenu(self):
+        # Loads JSON data each time we venture into the options menu, makes it easy to store data
+        with open('settings/game_settings.json', 'r') as loop:
+            data = json.load(loop)
+
+        with open('settings/game_settings.json', 'w') as loop:
+            data["Login Settings"]["Username"]  = self.name.text()
+            json.dump(data, loop, indent = 1)
+
+        # Assigns username data to JSON file    
+        self.name.text = data["Login Settings"]["Username"]
+
+        # Hides the window
+        self.hide()
+
+        # Opens the window
+        om = Options_Menu()
+        om.show()
+        om.exec_()
+        om.setWindowModality(QtCore.Qt.WindowModal)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+    qt_app = main_window()
+    qt_app.show()
+    app.exec_()
